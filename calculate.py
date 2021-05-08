7900c4491657159340f98d429e5dbfd7997df59b
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-05-08 14:46:17 +0500

    Добавлено консольное управление для пользователя

--- calculate.py
+++ calculate.py
@@ -1,5 +1,39 @@
-import core.polishCalculation as calculation
+import core.polishCalculation as calc
 import core.transformationToPolish as transform
-import math
 
-print(math.radians(90))
+
+def get_polish_notation(command):
+    if command == '1':
+        print('*** Выбран способ введения уравнения с переменной *** \n')
+        expression = input("Введите уравнение строго по образцу: 'A + B * cos ( 40 )' \n")
+        result = transform.transform_to_polish(expression)
+        print("Преобразование в польскую запись: " + result + '\n')
+        variable = input('Введите значение переменной:')
+        result_polish_notation = change_sym(result, 'X', variable)
+        answer = calc.calculate_polish(result_polish_notation)
+        print("Ответ: " + str(answer))
+
+    if command == '2':
+        print('*** Выбран способ введения уравнения без переменных *** \n')
+        expression = input("Введите уравнение строго по образцу: 'A + B * cos ( 40 )' \n")
+        result = transform.transform_to_polish(expression)
+        print("Преобразование в польскую запись: " + result + '\n')
+        answer = calc.calculate_polish(result)
+        print("Ответ: " + str(answer))
+
+
+def change_sym(ch, ca1, ca2):
+    b = ''
+    for x in ch:
+        if x != ca1:
+            b += x
+        else:
+            b += ca2
+    return b
+
+
+inpt = input("Выберите одну из возможных команд: \n "
+             "'уравнение с переменной' - 1 \n "
+             "'уравнение без перемменной' - 2 \n")
+
+get_polish_notation(inpt)
