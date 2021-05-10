d7965d3e5a14381f96b49fe5c8119e7fb7024737
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-05-10 23:42:15 +0500

    Удаление отладочной информации

--- core/polishCalculation.py
+++ core/polishCalculation.py
@@ -13,27 +13,13 @@ def arcctg(x):
 OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv, '^': math.pow,
              'div': operator.floordiv, '%': operator.mod}
 
-FUNCTIONS = {'cos': math.cos, 'sin': math.sin, 'tg': math.tan, 'arctg': math.atan, 'ctg': ctg,
+FUNCTIONS = {'cos': math.cos, 'arccos': math.acos, 'sin': math.sin, 'arcsin': math.asin, 'tg': math.tan,
+             'arctg': math.atan, 'ctg': ctg,
              'arcctg': arcctg, 'abs': abs, 'sqrt': math.sqrt, 'deg': math.radians, 'exp': math.exp, 'log': math.log}
 
 PRIORITY = {1: ['+', '-', ], 2: ['*', '/', 'div', '%'],
-            3: ['sin', 'cos', 'tg', 'artctg', 'ctg', 'arcctg', 'abs', 'sqrt', '^', 'deg', 'exp', 'log']}
+            3: ['sin', 'asin', 'cos', 'acos', 'tg', 'artctg', 'ctg', 'arcctg', 'abs', 'sqrt', '^', 'deg', 'exp', 'log']}
 
-
-# def polishCalculation(srt):
-#     stack = []
-#     str = srt
-#     lst = list(srt.split(' '))
-#     print(lst)
-#     for i in str:
-#         if i.isdigit():
-#             stack.append(i)
-#             lst.remove(i)
-#         else:
-#             cnt1, cnt2 = stack.pop(), stack.pop()
-#             stack.append(OPERATORS[i](int(cnt2), int(cnt1)))
-#             lst.remove(i)
-#     return stack.pop()
 
 def calculate_polish(str):
     stack = []
